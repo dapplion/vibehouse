dapplion/vibehouse

commit b4704eab4ac8edf0ea0282ed9a5758b784038dd2

Fulu update to spec v1.6.0-alpha.4 (#7890)
Fulu update to spec [v1.6.0-alpha.4](https://github.com/ethereum/consensus-specs/releases/tag/v1.6.0-alpha.4).
- Make `number_of_columns` a preset
- Optimise `get_custody_groups` to avoid computing if cgc = 128
- Add support for additional typenum values in type_dispatch macro

consensus/types/src/chain_spec.rs
@@ -201,7 +201,6 @@ pub struct ChainSpec {
     pub fulu_fork_version: [u8; 4],
     /// The Fulu fork epoch is optional, with `None` representing "Fulu never happens".
     pub fulu_fork_epoch: Option<Epoch>,
-    pub number_of_columns: u64,
     pub number_of_custody_groups: u64,
     pub data_column_sidecar_subnet_count: u64,
     pub samples_per_slot: u64,
@@ -773,20 +772,19 @@ impl ChainSpec {
     }
 
     /// Returns the number of data columns per custody group.
-    pub fn data_columns_per_group(&self) -> u64 {
-        self.number_of_columns
+    pub fn data_columns_per_group<E: EthSpec>(&self) -> u64 {
+        (E::number_of_columns() as u64)
             .safe_div(self.number_of_custody_groups)
             .expect("Custody group count must be greater than 0")
     }
 
     /// Returns the number of column sidecars to sample per slot.
-    pub fn sampling_size_columns(&self, custody_group_count: u64) -> Result<usize, String> {
+    pub fn sampling_size_columns<E: EthSpec>(
+        &self,
+        custody_group_count: u64,
+    ) -> Result<usize, String> {
         let sampling_size_groups = self.sampling_size_custody_groups(custody_group_count)?;
-
-        let columns_per_custody_group = self
-            .number_of_columns
-            .safe_div(self.number_of_custody_groups)
-            .map_err(|_| "number_of_custody_groups must be greater than 0")?;
+        let columns_per_custody_group = self.data_columns_per_group::<E>();
 
         let sampling_size_columns = columns_per_custody_group
             .safe_mul(sampling_size_groups)
@@ -1048,7 +1046,6 @@ impl ChainSpec {
             custody_requirement: 4,
             number_of_custody_groups: 128,
             data_column_sidecar_subnet_count: 128,
-            number_of_columns: 128,
             samples_per_slot: 8,
             validator_custody_requirement: 8,
             balance_per_additional_custody_group: 32000000000,
@@ -1088,7 +1085,6 @@ impl ChainSpec {
             max_blocks_by_root_request: default_max_blocks_by_root_request(),
             max_blocks_by_root_request_deneb: default_max_blocks_by_root_request_deneb(),
             max_blobs_by_root_request: default_max_blobs_by_root_request(),
-            max_data_columns_by_root_request: default_data_columns_by_root_request(),
 
             /*
              * Networking Electra specific
@@ -1103,6 +1099,7 @@ impl ChainSpec {
             blob_schedule: BlobSchedule::default(),
             min_epochs_for_data_column_sidecars_requests:
                 default_min_epochs_for_data_column_sidecars_requests(),
+            max_data_columns_by_root_request: default_data_columns_by_root_request(),
 
             /*
              * Application specific
@@ -1386,7 +1383,6 @@ impl ChainSpec {
             custody_requirement: 4,
             number_of_custody_groups: 128,
             data_column_sidecar_subnet_count: 128,
-            number_of_columns: 128,
             samples_per_slot: 8,
             validator_custody_requirement: 8,
             balance_per_additional_custody_group: 32000000000,
@@ -1426,7 +1422,6 @@ impl ChainSpec {
             max_blocks_by_root_request: default_max_blocks_by_root_request(),
             max_blocks_by_root_request_deneb: default_max_blocks_by_root_request_deneb(),
             max_blobs_by_root_request: default_max_blobs_by_root_request(),
-            max_data_columns_by_root_request: default_data_columns_by_root_request(),
 
             /*
              * Networking Electra specific
@@ -1441,6 +1436,7 @@ impl ChainSpec {
             blob_schedule: BlobSchedule::default(),
             min_epochs_for_data_column_sidecars_requests:
                 default_min_epochs_for_data_column_sidecars_requests(),
+            max_data_columns_by_root_request: default_data_columns_by_root_request(),
 
             /*
              * Application specific
@@ -1759,9 +1755,6 @@ pub struct Config {
     #[serde(with = "serde_utils::quoted_u64")]
     max_request_blob_sidecars_electra: u64,
 
-    #[serde(default = "default_number_of_columns")]
-    #[serde(with = "serde_utils::quoted_u64")]
-    number_of_columns: u64,
     #[serde(default = "default_number_of_custody_groups")]
     #[serde(with = "serde_utils::quoted_u64")]
     number_of_custody_groups: u64,
@@ -1938,10 +1931,6 @@ const fn default_data_column_sidecar_subnet_count() -> u64 {
     128
 }
 
-const fn default_number_of_columns() -> u64 {
-    128
-}
-
 const fn default_number_of_custody_groups() -> u64 {
     128
 }
@@ -1987,19 +1976,15 @@ fn max_blobs_by_root_request_common(max_request_blob_sidecars: u64) -> usize {
     .len()
 }
 
-fn max_data_columns_by_root_request_common(
-    max_request_blocks: u64,
-    number_of_columns: u64,
-) -> usize {
+fn max_data_columns_by_root_request_common<E: EthSpec>(max_request_blocks: u64) -> usize {
     let max_request_blocks = max_request_blocks as usize;
-    let number_of_columns = number_of_columns as usize;
 
     let empty_data_columns_by_root_id = DataColumnsByRootIdentifier {
         block_root: Hash256::zero(),
-        columns: RuntimeVariableList::from_vec(vec![0; number_of_columns], number_of_columns),
+        columns: VariableList::from(vec![0]),
     };
 
-    RuntimeVariableList::<DataColumnsByRootIdentifier>::from_vec(
+    RuntimeVariableList::<DataColumnsByRootIdentifier<E>>::from_vec(
         vec![empty_data_columns_by_root_id; max_request_blocks],
         max_request_blocks,
     )
@@ -2020,10 +2005,7 @@ fn default_max_blobs_by_root_request() -> usize {
 }
 
 fn default_data_columns_by_root_request() -> usize {
-    max_data_columns_by_root_request_common(
-        default_max_request_blocks_deneb(),
-        default_number_of_columns(),
-    )
+    max_data_columns_by_root_request_common::<MainnetEthSpec>(default_max_request_blocks_deneb())
 }
 
 impl Default for Config {
@@ -2165,7 +2147,6 @@ impl Config {
             blob_sidecar_subnet_count_electra: spec.blob_sidecar_subnet_count_electra,
             max_request_blob_sidecars_electra: spec.max_request_blob_sidecars_electra,
 
-            number_of_columns: spec.number_of_columns,
             number_of_custody_groups: spec.number_of_custody_groups,
             data_column_sidecar_subnet_count: spec.data_column_sidecar_subnet_count,
             samples_per_slot: spec.samples_per_slot,
@@ -2248,7 +2229,6 @@ impl Config {
             max_blobs_per_block_electra,
             blob_sidecar_subnet_count_electra,
             max_request_blob_sidecars_electra,
-            number_of_columns,
             number_of_custody_groups,
             data_column_sidecar_subnet_count,
             samples_per_slot,
@@ -2330,12 +2310,10 @@ impl Config {
                 max_request_blocks_deneb,
             ),
             max_blobs_by_root_request: max_blobs_by_root_request_common(max_request_blob_sidecars),
-            max_data_columns_by_root_request: max_data_columns_by_root_request_common(
+            max_data_columns_by_root_request: max_data_columns_by_root_request_common::<E>(
                 max_request_blocks_deneb,
-                number_of_columns,
             ),
 
-            number_of_columns,
             number_of_custody_groups,
             data_column_sidecar_subnet_count,
             samples_per_slot,
@@ -2815,7 +2793,6 @@ mod yaml_tests {
         DEPOSIT_CONTRACT_ADDRESS: 0x00000000219ab540356cBB839Cbe05303d7705Fa
         CUSTODY_REQUIREMENT: 1
         DATA_COLUMN_SIDECAR_SUBNET_COUNT: 128
-        NUMBER_OF_COLUMNS: 128
         SAMPLES_PER_SLOT: 8
         "#;
 

beacon_node/beacon_chain/benches/benches.rs
@@ -7,7 +7,7 @@ use criterion::{Criterion, black_box, criterion_group, criterion_main};
 use bls::Signature;
 use kzg::{KzgCommitment, KzgProof};
 use types::{
-    BeaconBlock, BeaconBlockDeneb, Blob, BlobsList, ChainSpec, EmptyBlock, EthSpec, KzgProofs,
+    BeaconBlock, BeaconBlockFulu, Blob, BlobsList, ChainSpec, EmptyBlock, EthSpec, KzgProofs,
     MainnetEthSpec, SignedBeaconBlock, beacon_block_body::KzgCommitments,
 };
 
@@ -15,7 +15,7 @@ fn create_test_block_and_blobs<E: EthSpec>(
     num_of_blobs: usize,
     spec: &ChainSpec,
 ) -> (SignedBeaconBlock<E>, BlobsList<E>, KzgProofs<E>) {
-    let mut block = BeaconBlock::Deneb(BeaconBlockDeneb::empty(spec));
+    let mut block = BeaconBlock::Fulu(BeaconBlockFulu::empty(spec));
     let mut body = block.body_mut();
     let blob_kzg_commitments = body.blob_kzg_commitments_mut().unwrap();
     *blob_kzg_commitments =
@@ -27,7 +27,7 @@ fn create_test_block_and_blobs<E: EthSpec>(
         .map(|_| Blob::<E>::default())
         .collect::<Vec<_>>()
         .into();
-    let proofs = vec![KzgProof::empty(); num_of_blobs * spec.number_of_columns as usize].into();
+    let proofs = vec![KzgProof::empty(); num_of_blobs * E::number_of_columns()].into();
 
     (signed_block, blobs, proofs)
 }

beacon_node/beacon_chain/src/beacon_chain.rs
@@ -1247,8 +1247,8 @@ impl<T: BeaconChainTypes> BeaconChain<T> {
 
         if self.spec.is_peer_das_enabled_for_epoch(block.epoch()) {
             if let Some(columns) = self.store.get_data_columns(block_root)? {
-                let num_required_columns = self.spec.number_of_columns / 2;
-                let reconstruction_possible = columns.len() >= num_required_columns as usize;
+                let num_required_columns = T::EthSpec::number_of_columns() / 2;
+                let reconstruction_possible = columns.len() >= num_required_columns;
                 if reconstruction_possible {
                     reconstruct_blobs(&self.kzg, &columns, None, &block, &self.spec)
                         .map(Some)

beacon_node/beacon_chain/src/block_verification_types.rs
@@ -3,13 +3,14 @@ pub use crate::data_availability_checker::{AvailableBlock, MaybeAvailableBlock};
 use crate::data_column_verification::{CustodyDataColumn, CustodyDataColumnList};
 use crate::{PayloadVerificationOutcome, get_block_root};
 use derivative::Derivative;
+use ssz_types::VariableList;
 use state_processing::ConsensusContext;
 use std::fmt::{Debug, Formatter};
 use std::sync::Arc;
 use types::blob_sidecar::BlobIdentifier;
 use types::{
-    BeaconBlockRef, BeaconState, BlindedPayload, BlobSidecarList, ChainSpec, Epoch, EthSpec,
-    Hash256, RuntimeVariableList, SignedBeaconBlock, SignedBeaconBlockHeader, Slot,
+    BeaconBlockRef, BeaconState, BlindedPayload, BlobSidecarList, Epoch, EthSpec, Hash256,
+    SignedBeaconBlock, SignedBeaconBlockHeader, Slot,
 };
 
 /// A block that has been received over RPC. It has 2 internal variants:
@@ -151,7 +152,6 @@ impl<E: EthSpec> RpcBlock<E> {
         block_root: Option<Hash256>,
         block: Arc<SignedBeaconBlock<E>>,
         custody_columns: Vec<CustodyDataColumn<E>>,
-        spec: &ChainSpec,
     ) -> Result<Self, AvailabilityCheckError> {
         let block_root = block_root.unwrap_or_else(|| get_block_root(&block));
 
@@ -161,10 +161,7 @@ impl<E: EthSpec> RpcBlock<E> {
         }
         // Treat empty data column lists as if they are missing.
         let inner = if !custody_columns.is_empty() {
-            RpcBlockInner::BlockAndCustodyColumns(
-                block,
-                RuntimeVariableList::new(custody_columns, spec.number_of_columns as usize)?,
-            )
+            RpcBlockInner::BlockAndCustodyColumns(block, VariableList::new(custody_columns)?)
         } else {
             RpcBlockInner::Block(block)
         };

beacon_node/beacon_chain/src/data_availability_checker.rs
@@ -446,7 +446,7 @@ impl<T: BeaconChainTypes> DataAvailabilityChecker<T> {
             .map(CustodyDataColumn::into_inner)
             .collect::<Vec<_>>();
         let all_data_columns =
-            RuntimeVariableList::from_vec(all_data_columns, self.spec.number_of_columns as usize);
+            RuntimeVariableList::from_vec(all_data_columns, T::EthSpec::number_of_columns());
 
         // verify kzg for all data columns at once
         if !all_data_columns.is_empty() {

beacon_node/beacon_chain/src/data_availability_checker/overflow_lru_cache.rs
@@ -591,7 +591,7 @@ impl<T: BeaconChainTypes> DataAvailabilityCheckerInner<T> {
         };
 
         // If we're sampling all columns, it means we must be custodying all columns.
-        let total_column_count = self.spec.number_of_columns as usize;
+        let total_column_count = T::EthSpec::number_of_columns();
         let received_column_count = pending_components.verified_data_columns.len();
 
         if pending_components.reconstruction_started {

beacon_node/beacon_chain/src/data_column_verification.rs
@@ -12,6 +12,7 @@ use kzg::{Error as KzgError, Kzg};
 use proto_array::Block;
 use slot_clock::SlotClock;
 use ssz_derive::{Decode, Encode};
+use ssz_types::VariableList;
 use std::iter;
 use std::marker::PhantomData;
 use std::sync::Arc;
@@ -19,7 +20,7 @@ use tracing::{debug, instrument};
 use types::data_column_sidecar::ColumnIndex;
 use types::{
     BeaconStateError, ChainSpec, DataColumnSidecar, DataColumnSubnetId, EthSpec, Hash256,
-    RuntimeVariableList, SignedBeaconBlockHeader, Slot,
+    SignedBeaconBlockHeader, Slot,
 };
 
 /// An error occurred while validating a gossip data column.
@@ -315,7 +316,8 @@ impl<E: EthSpec> KzgVerifiedDataColumn<E> {
     }
 }
 
-pub type CustodyDataColumnList<E> = RuntimeVariableList<CustodyDataColumn<E>>;
+pub type CustodyDataColumnList<E> =
+    VariableList<CustodyDataColumn<E>, <E as EthSpec>::NumberOfColumns>;
 
 /// Data column that we must custody
 #[derive(Debug, Derivative, Clone, Encode, Decode)]
@@ -479,7 +481,7 @@ pub fn validate_data_column_sidecar_for_gossip<T: BeaconChainTypes, O: Observati
     chain: &BeaconChain<T>,
 ) -> Result<GossipVerifiedDataColumn<T, O>, GossipDataColumnError> {
     let column_slot = data_column.slot();
-    verify_data_column_sidecar(&data_column, &chain.spec)?;
+    verify_data_column_sidecar(&data_column)?;
     verify_index_matches_subnet(&data_column, subnet, &chain.spec)?;
     verify_sidecar_not_from_future_slot(chain, column_slot)?;
     verify_slot_greater_than_latest_finalized_slot(chain, column_slot)?;
@@ -533,9 +535,8 @@ pub fn validate_data_column_sidecar_for_gossip<T: BeaconChainTypes, O: Observati
 /// Verify if the data column sidecar is valid.
 fn verify_data_column_sidecar<E: EthSpec>(
     data_column: &DataColumnSidecar<E>,
-    spec: &ChainSpec,
 ) -> Result<(), GossipDataColumnError> {
-    if data_column.index >= spec.number_of_columns {
+    if data_column.index >= E::number_of_columns() as u64 {
         return Err(GossipDataColumnError::InvalidColumnIndex(data_column.index));
     }
     if data_column.kzg_commitments.is_empty() {

commit 747d9118ff4c487bb28323b1bf16a184a56be102

Fix `DataColumnsByRoot` request limit validation bug (#7928)
Fixes #7926

This was a bug I introduced in #7890 and @pawanjay176 noticed it on some running nodes, and added a rpc test to confirm it.

The culprit is this line, where I failed to fill the vec to it's max size, so it doesn't calculate the max size properly, resulting in all `DataColumnByRoot` requests exceeding the max size during validation:
https://github.com/sigp/lighthouse/blob/d24a6d2a45f25dfdba8740d10c569226555a8143/consensus/types/src/chain_spec.rs#L1984

The PR fixes this and includes new regression tests for this fix.

beacon_node/lighthouse_network/tests/rpc_tests.rs
@@ -15,9 +15,10 @@ use tokio::runtime::Runtime;
 use tokio::time::sleep;
 use tracing::{Instrument, debug, error, info_span, warn};
 use types::{
-    BeaconBlock, BeaconBlockAltair, BeaconBlockBase, BeaconBlockBellatrix, BlobSidecar, ChainSpec,
-    EmptyBlock, Epoch, EthSpec, FixedBytesExtended, ForkName, Hash256, MinimalEthSpec,
-    RuntimeVariableList, Signature, SignedBeaconBlock, Slot,
+    BeaconBlock, BeaconBlockAltair, BeaconBlockBase, BeaconBlockBellatrix, BeaconBlockHeader,
+    BlobSidecar, ChainSpec, DataColumnSidecar, DataColumnsByRootIdentifier, EmptyBlock, Epoch,
+    EthSpec, FixedBytesExtended, ForkName, Hash256, KzgCommitment, KzgProof, MinimalEthSpec,
+    RuntimeVariableList, Signature, SignedBeaconBlock, SignedBeaconBlockHeader, Slot,
 };
 
 type E = MinimalEthSpec;
@@ -932,6 +933,307 @@ fn test_tcp_blocks_by_root_chunked_rpc() {
                                 Response::BlocksByRange(None),
                             );
                             debug!("Send stream term");
+                        }
+                    }
+                    _ => {} // Ignore other events
+                }
+            }
+        }
+        .instrument(info_span!("Receiver"));
+        tokio::select! {
+            _ = sender_future => {}
+            _ = receiver_future => {}
+            _ = sleep(Duration::from_secs(300)) => {
+                    panic!("Future timed out");
+            }
+        }
+    })
+}
+
+#[test]
+#[allow(clippy::single_match)]
+fn test_tcp_columns_by_root_chunked_rpc() {
+    // Set up the logging.
+    let log_level = "debug";
+    let enable_logging = true;
+    let _subscriber = build_tracing_subscriber(log_level, enable_logging);
+    let num_of_columns = E::number_of_columns();
+    let messages_to_send = 32 * num_of_columns;
+
+    let spec = Arc::new(spec_with_all_forks_enabled());
+    let current_fork_name = ForkName::Fulu;
+
+    let rt = Arc::new(Runtime::new().unwrap());
+    // get sender/receiver
+    rt.block_on(async {
+        let (mut sender, mut receiver) = common::build_node_pair(
+            Arc::downgrade(&rt),
+            current_fork_name,
+            spec.clone(),
+            Protocol::Tcp,
+            false,
+            None,
+        )
+        .await;
+
+        // DataColumnsByRootRequest Request
+
+        let max_request_blocks = spec.max_request_blocks(current_fork_name);
+        let req = DataColumnsByRootRequest::new(
+            vec![
+                DataColumnsByRootIdentifier {
+                    block_root: Hash256::zero(),
+                    columns: VariableList::new(
+                        (0..E::number_of_columns() as u64).collect::<Vec<_>>()
+                    )
+                    .unwrap(),
+                };
+                max_request_blocks
+            ],
+            max_request_blocks,
+        );
+        let req_bytes = req.data_column_ids.as_ssz_bytes();
+        let req_decoded = DataColumnsByRootRequest {
+            data_column_ids: <RuntimeVariableList<DataColumnsByRootIdentifier<E>>>::from_ssz_bytes(
+                &req_bytes,
+                spec.max_request_blocks(current_fork_name),
+            )
+            .unwrap(),
+        };
+        assert_eq!(req, req_decoded);
+        let rpc_request = RequestType::DataColumnsByRoot(req);
+
+        // DataColumnsByRoot Response
+        let data_column = Arc::new(DataColumnSidecar {
+            index: 1,
+            signed_block_header: SignedBeaconBlockHeader {
+                message: BeaconBlockHeader {
+                    slot: 320u64.into(),
+                    proposer_index: 1,
+                    parent_root: Hash256::zero(),
+                    state_root: Hash256::zero(),
+                    body_root: Hash256::zero(),
+                },
+                signature: Signature::empty(),
+            },
+            column: vec![vec![0; E::bytes_per_blob()].into()].into(),
+            kzg_commitments: vec![KzgCommitment::empty_for_testing()].into(),
+            kzg_proofs: vec![KzgProof::empty()].into(),
+            kzg_commitments_inclusion_proof: vec![
+                Hash256::zero();
+                E::kzg_commitments_inclusion_proof_depth()
+            ]
+            .into(),
+        });
+
+        let rpc_response = Response::DataColumnsByRoot(Some(data_column.clone()));
+
+        // keep count of the number of messages received
+        let mut messages_received = 0;
+        // build the sender future
+        let sender_future = async {
+            loop {
+                match sender.next_event().await {
+                    NetworkEvent::PeerConnectedOutgoing(peer_id) => {
+                        tracing::info!("Sending RPC");
+                        tokio::time::sleep(Duration::from_secs(1)).await;
+                        sender
+                            .send_request(peer_id, AppRequestId::Router, rpc_request.clone())
+                            .unwrap();
+                    }
+                    NetworkEvent::ResponseReceived {
+                        peer_id: _,
+                        app_request_id: AppRequestId::Router,
+                        response,
+                    } => match response {
+                        Response::DataColumnsByRoot(Some(sidecar)) => {
+                            assert_eq!(sidecar, data_column.clone());
+                            messages_received += 1;
+                            tracing::info!("Chunk received");
+                        }
+                        Response::DataColumnsByRoot(None) => {
+                            // should be exactly messages_to_send
+                            assert_eq!(messages_received, messages_to_send);
+                            // end the test
+                            return;
+                        }
+                        _ => {} // Ignore other RPC messages
+                    },
+                    _ => {} // Ignore other behaviour events
+                }
+            }
+        }
+        .instrument(info_span!("Sender"));
+
+        // build the receiver future
+        let receiver_future = async {
+            loop {
+                match receiver.next_event().await {
+                    NetworkEvent::RequestReceived {
+                        peer_id,
+                        inbound_request_id,
+                        request_type,
+                    } => {
+                        if request_type == rpc_request {
+                            // send the response
+                            tracing::info!("Receiver got request");
+
+                            for _ in 0..messages_to_send {
+                                receiver.send_response(
+                                    peer_id,
+                                    inbound_request_id,
+                                    rpc_response.clone(),
+                                );
+                                tracing::info!("Sending message");
+                            }
+                            // send the stream termination
+                            receiver.send_response(
+                                peer_id,
+                                inbound_request_id,
+                                Response::DataColumnsByRoot(None),
+                            );
+                            tracing::info!("Send stream term");
+                        }
+                    }
+                    e => {
+                        tracing::info!(?e, "Got event");
+                    } // Ignore other events
+                }
+            }
+        }
+        .instrument(info_span!("Receiver"));
+        tokio::select! {
+            _ = sender_future => {}
+            _ = receiver_future => {}
+            _ = sleep(Duration::from_secs(300)) => {
+                    panic!("Future timed out");
+            }
+        }
+    })
+}
+
+#[test]
+#[allow(clippy::single_match)]
+fn test_tcp_columns_by_range_chunked_rpc() {
+    // Set up the logging.
+    let log_level = "debug";
+    let enable_logging = true;
+    let _subscriber = build_tracing_subscriber(log_level, enable_logging);
+
+    let messages_to_send = 32;
+
+    let spec = Arc::new(spec_with_all_forks_enabled());
+    let current_fork_name = ForkName::Fulu;
+
+    let rt = Arc::new(Runtime::new().unwrap());
+    // get sender/receiver
+    rt.block_on(async {
+        let (mut sender, mut receiver) = common::build_node_pair(
+            Arc::downgrade(&rt),
+            current_fork_name,
+            spec.clone(),
+            Protocol::Tcp,
+            false,
+            None,
+        )
+        .await;
+
+        // DataColumnsByRange Request
+        let rpc_request = RequestType::DataColumnsByRange(DataColumnsByRangeRequest {
+            start_slot: 320,
+            count: 32,
+            columns: (0..E::number_of_columns() as u64).collect(),
+        });
+
+        // DataColumnsByRange Response
+        let data_column = Arc::new(DataColumnSidecar {
+            index: 1,
+            signed_block_header: SignedBeaconBlockHeader {
+                message: BeaconBlockHeader {
+                    slot: 320u64.into(),
+                    proposer_index: 1,
+                    parent_root: Hash256::zero(),
+                    state_root: Hash256::zero(),
+                    body_root: Hash256::zero(),
+                },
+                signature: Signature::empty(),
+            },
+            column: vec![vec![0; E::bytes_per_blob()].into()].into(),
+            kzg_commitments: vec![KzgCommitment::empty_for_testing()].into(),
+            kzg_proofs: vec![KzgProof::empty()].into(),
+            kzg_commitments_inclusion_proof: vec![
+                Hash256::zero();
+                E::kzg_commitments_inclusion_proof_depth()
+            ]
+            .into(),
+        });
+
+        let rpc_response = Response::DataColumnsByRange(Some(data_column.clone()));
+
+        // keep count of the number of messages received
+        let mut messages_received = 0;
+        // build the sender future
+        let sender_future = async {
+            loop {
+                match sender.next_event().await {
+                    NetworkEvent::PeerConnectedOutgoing(peer_id) => {
+                        tracing::info!("Sending RPC");
+                        sender
+                            .send_request(peer_id, AppRequestId::Router, rpc_request.clone())
+                            .unwrap();
+                    }
+                    NetworkEvent::ResponseReceived {
+                        peer_id: _,
+                        app_request_id: AppRequestId::Router,
+                        response,
+                    } => match response {
+                        Response::DataColumnsByRange(Some(sidecar)) => {
+                            assert_eq!(sidecar, data_column.clone());
+                            messages_received += 1;
+                            tracing::info!("Chunk received");
+                        }
+                        Response::DataColumnsByRange(None) => {
+                            // should be exactly messages_to_send
+                            assert_eq!(messages_received, messages_to_send);
+                            // end the test
+                            return;
+                        }
+                        _ => {} // Ignore other RPC messages
+                    },
+                    _ => {} // Ignore other behaviour events
+                }
+            }
+        }
+        .instrument(info_span!("Sender"));
+
+        // build the receiver future
+        let receiver_future = async {
+            loop {
+                match receiver.next_event().await {
+                    NetworkEvent::RequestReceived {
+                        peer_id,
+                        inbound_request_id,
+                        request_type,
+                    } => {
+                        if request_type == rpc_request {
+                            // send the response
+                            tracing::info!("Receiver got request");
+
+                            for _ in 0..messages_to_send {
+                                receiver.send_response(
+                                    peer_id,
+                                    inbound_request_id,
+                                    rpc_response.clone(),
+                                );
+                                tracing::info!("Sending message");
+                            }
+                            // send the stream termination
+                            receiver.send_response(
+                                peer_id,
+                                inbound_request_id,
+                                Response::DataColumnsByRange(None),
+                            );
+                            tracing::info!("Send stream term");
                         }
                     }
                     _ => {} // Ignore other events

consensus/types/src/chain_spec.rs
@@ -1981,7 +1981,7 @@ fn max_data_columns_by_root_request_common<E: EthSpec>(max_request_blocks: u64) 
 
     let empty_data_columns_by_root_id = DataColumnsByRootIdentifier {
         block_root: Hash256::zero(),
-        columns: VariableList::from(vec![0]),
+        columns: VariableList::from(vec![0; E::number_of_columns()]),
     };
 
     RuntimeVariableList::<DataColumnsByRootIdentifier<E>>::from_vec(